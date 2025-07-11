b095309232b3995f8b58d49c78fdd226c6c19756
Brayan Duque <bradumar2016@gmail.com>
2025-07-11 13:24:51 -0500

    instalación del entorno

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className='text-amber-500 text-5xl font-extrabold'>Hola mundo acá creare la api de rick y morty</p>
+    </>
+  )
+}
+
+export default App
